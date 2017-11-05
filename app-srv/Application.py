ad0cdb4d82b7d7d7a215a8d13f1b9a1aea45484a
Amit Kulkarni <amitk@wepay.com>
2017-11-05 15:46:40 -0800

    Add agency route to agency resource

--- app-srv/Application.py
+++ app-srv/Application.py
@@ -3,6 +3,7 @@ import falcon
 import logging
 from app.setup import APP_NAME, configure_logger
 from app.resources.basic import Ping, Hello
+from app.resources.agency import Agency
 
 configure_logger()
 logger = logging.getLogger(APP_NAME)
@@ -11,3 +12,4 @@ app = falcon.API()
 # list of routes
 app.add_route("/", Hello())
 app.add_route("/ping", Ping())
+app.add_route("/agency", Agency())
