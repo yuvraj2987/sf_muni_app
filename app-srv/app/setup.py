36489b6fb599ffd303265face8d36483a7b15972
Amit Kulkarni <amitk@wepay.com>
2017-11-05 01:45:51 -0700

    Add logger config

---
+++ app-srv/app/setup.py
@@ -0,0 +1,20 @@
+"""
+    Setup app before it starts
+"""
+
+import logging
+
+APP_NAME = "Sf_Muni_App"
+
+def configure_logger():
+    """
+        Configure logger to be used by app
+    """
+    logger = logging.getLogger(APP_NAME)
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter("%(asctime)s|"
+                                  + "%(name)s| %(levelname)-2s|"
+                                  + "%(message)s")
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.setLevel(logging.DEBUG)
