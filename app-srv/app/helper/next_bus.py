bc13362be7db174d88c6bfaf2fcc38f89779d218
Amit Kulkarni <amitk@wepay.com>
2017-11-05 15:42:56 -0800

    Add next bus helper utility
    Add get_bus_data method to fetch data from next bus api.
    
    Parses command output and converts xml to json data.

---
+++ app-srv/app/helper/next_bus.py
@@ -0,0 +1,50 @@
+"""
+    Utilities for call NextBus XML server
+    and return json object
+"""
+import requests
+import logging
+from xml.etree import ElementTree as ET
+from app.setup import APP_NAME
+
+
+logger = logging.get_logger(APP_NAME)
+
+NEXT_URL = "http://webservices.nextbus.com/service/publicXMLFeed"
+
+
+def get_bus_data(cmd, arg=None):
+    """
+        Return json data for xml feed
+        @input:
+            cmd = string
+            args = list of arguments to command
+        @output:
+            json data or None
+    """
+    if cmd is None:
+        return None
+    cmd_url = "{}?{}".format(NEXT_URL, full_cmd)
+    logger.debug("command url %s", cmd_url)
+    try:
+        resp = requests.get(cmd_url)
+        json_data = xml_to_json(resp.content)
+        return json_data
+    # Todo: Remove generic excpetion handler
+    except Exception as e:
+        logger.error("Exception while fetching data %s", str(e))
+        return None
+
+# end of method
+
+
+def xml_to_json(xml_str):
+    """
+        Convet xml tree to json data
+    """
+    root = ET.fromstring(xml_str)
+    ret_data = []
+    for child in root:
+        ret_data.append(child.attrib)
+    # end of for
+    return ret_data
