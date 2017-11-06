4a6854c5b580419e3d2b80ad1fc25d4f2d6b7901
Amit Kulkarni <amitk@wepay.com>
2017-11-06 14:10:32 -0800

    Use Redis in agency resource class

--- app-srv/app/resources/agency.py
+++ app-srv/app/resources/agency.py
@@ -3,6 +3,7 @@ import logging
 import json
 from app.helper.next_bus import get_bus_data
 from app.setup import APP_NAME
+from app.storage import RedisClient
 
 
 logger = logging.getLogger(APP_NAME)
@@ -12,12 +13,24 @@ class Agency(object):
     """
         Manage agency resource
     """
+    _redis = RedisClient()
+
     def on_get(self, req, resp):
         """
             Get list of agencies
         """
         logger.info("get agency list")
-        data = get_bus_data("agencyList")
+        logger.debug("request path %s", req.path)
+        if self._redis.exists(req.path):
+            logger.info("%s key found in cache", req.path)
+            data = self._redis.get(req.path)
+        else:
+            logger.info("%s key not found in cache", req.path)
+            data = get_bus_data("agencyList")
+            if data is not None:
+                logger.info("%s key inserted into cache", req.path)
+                self._redis.set(req.path, json.dumps(data))
+        #
         if data is None:
             logger.error("failed to get list for next bus")
             resp.status = falcon.HTTP_NO_CONTENT
@@ -28,5 +41,3 @@ class Agency(object):
     # end of method
 
 # end of class
-
-
