2aaab977d08948cd8d6a4ff0fef2f08effe07d71
Amit Kulkarni <amitk@wepay.com>
2017-11-06 14:09:48 -0800

    Configure RedisClient before using it in app

--- app-srv/Application.py
+++ app-srv/Application.py
@@ -4,10 +4,13 @@ import logging
 from app.setup import APP_NAME, configure_logger
 from app.resources.basic import Ping, Hello
 from app.resources.agency import Agency
+from app.storage import RedisClient
 
 configure_logger()
 logger = logging.getLogger(APP_NAME)
 logger.info("Setup Falcon app")
+logger.info("Configure redis client")
+RedisClient.setup("redis", 6379)
 app = falcon.API()
 # list of routes
 app.add_route("/", Hello())
